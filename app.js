4104259cd086620cb0ff9b5051b5c5190f91e90a
kononovich11 <kononovich1703@gmail.com>
2020-02-26 17:33:18 +0300

    different tasks on string methods

--- app.js
+++ app.js
@@ -38,7 +38,43 @@ function extractCurrencyValue(str) {
     for (let i = 0; i < str.length; i++) {
         (isFinite(str[i])) ? newStr += str[i] : i;
     }
-    return +newStr;
+    return + newStr;
 }
 
 console.log(extractCurrencyValue('$120'));
+
+/*
+let string = ‘some test string’;
+1. Получить первую и последнюю буквы строки
+2. Сделать первую и последнюю буквы в верхнем регистре
+3. Найти положение слова ‘string’ в строке
+4. Найти положение второго пробела (“вручную” ничего не считать)
+5. Получить строку с 5-го символа длиной 4 буквы
+6. Получить строку с 5-го по 9-й символы
+7. Получить новую строку из исходной путем удаления последних 6-и символов
+8. Из двух переменных a=20 и b=16 получить переменную string, в которой будет
+содержаться текст “2016”
+*/
+
+let string = 'some test string';
+
+// 1
+let firstLetter = string[0];
+let lastLetter = string[string.length - 1];
+// 2
+firstLetter = firstLetter.toUpperCase();
+lastLetter = lastLetter.toUpperCase();
+// 3
+const positionString = string.indexOf('string');
+// 4
+const firstGap = string.indexOf(' ');
+const secondGap = string.indexOf(' ',firstGap + 1); 
+//5
+const strSubstr = string.substr(5, 4);
+//6
+const strSlice = string.slice(5,9);
+//7
+const newString = string.slice(0,string.length - 6);
+//8
+const a = 20, b = 16;
+const strConcat = a.toString() + b;
