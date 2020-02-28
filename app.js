a2d506e586e17130ad33c949351659004fb2a575
kononovich11 <kononovich1703@gmail.com>
2020-02-28 16:59:01 +0300

    array of names

--- app.js
+++ app.js
@@ -153,3 +153,24 @@ function copySorted(arr) {
     return newArr.sort();
 }
 let sorted = copySorted(arr3);
+
+/*
+У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
+
+Например:
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+alert( names ); // Вася, Петя, Маша
+*/
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+let names = users.map(item => item.name);
