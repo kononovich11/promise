6886062ff38ae60f9b3cb8c60e4f8b6a38ca8939
kononovich11 <kononovich1703@gmail.com>
2020-02-28 13:03:38 +0300

    work with methods split, join, map

--- app.js
+++ app.js
@@ -49,3 +49,20 @@ function sumInput() {
 }
 
 console.log(sumInput());
+
+/*
+Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+
+То есть дефисы удаляются, а все слова после них получают заглавную букву.
+
+Примеры:
+
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
+*/
+function camelize(str) {
+    return str.split('-').map((item, index) => index === 0 ? item : item[0].toUpperCase() + item.slice(1)).join('');
+}
+
+console.log(camelize('-webkit-transition'));
