af6d4ba159f9002aeb404d002813b1932606b985
kononovich11 <kononovich1703@gmail.com>
2020-02-28 14:24:49 +0300

    Method filter

--- app.js
+++ app.js
@@ -66,3 +66,29 @@ function camelize(str) {
 }
 
 console.log(camelize('-webkit-transition'));
+
+/*
+Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
+
+Функция должна возвращать новый массив и не изменять исходный.
+
+Например:
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (совпадающие значения)
+
+alert( arr ); // 5,3,8,1 (без изменений)
+*/
+function filterRange(arr, a, b) {
+    return arr.filter(item => {
+        let filterVal = (a != undefined && b != undefined) ? item >= a && item <= b : arr;
+        return filterVal;
+    });
+}
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr);
+console.log(filtered);
