91996b4fdae855368df6232188075b19a6aa1c11
kononovich11 <kononovich1703@gmail.com>
2020-03-03 18:24:01 +0300

    find object by prop

--- app.js
+++ app.js
@@ -260,11 +260,56 @@ let doubleArr = doubleArray([1, 2, 3]);
 */
 function changeCollection() {
     let generalArr = [];
-    for(let i = 0; i< arguments.length; i++){
-       arguments[i].shift();
-       generalArr[i] = arguments[i]; 
+    for (let i = 0; i < arguments.length; i++) {
+        arguments[i].shift();
+        generalArr[i] = arguments[i];
     }
-   return generalArr;
+    return generalArr;
 }
 
-console.log(changeCollection([1,2,3],['a','b','c']));
+console.log(changeCollection([
+    1, 2, 3
+], ['a', 'b', 'c']));
+
+/*
+Создать функцию которая принимает массив пользователей, поле которое хочу проверить и значение на которое хочу проверять указанное поле.Возвращать новый массив с пользователями соответствующие указанным параметрам.
+*/
+const users1 = [
+    {
+        "_id": "5e36b779dc76fe3db02adc32",
+        "balance": "$1,955.65",
+        "picture": "http://placehold.it/32x32",
+        "age": 33,
+        "name": "Berg Zimmerman",
+        "gender": "male"
+    }, {
+        "_id": "5e36b779d117774176f90e0b",
+        "balance": "$3,776.14",
+        "picture": "http://placehold.it/32x32",
+        "age": 37,
+        "name": "Deann Winters",
+        "gender": "female"
+    }, {
+        "_id": "5e36b779daf6e455ec54cf45",
+        "balance": "$3,424.84",
+        "picture": "http://placehold.it/32x32",
+        "age": 36,
+        "name": "Kari Waters",
+        "gender": "female"
+    }
+]
+
+
+function filterUsers(arr, key, value) {
+    let findObjArr = [];
+    arr.forEach(item => {
+        for (let prop in item) {
+            if (prop == key && item[prop] == value) {
+                findObjArr = item;
+            }
+        }
+    });
+    return findObjArr;
+}
+
+console.log(filterUsers(users1, "age", 36));
