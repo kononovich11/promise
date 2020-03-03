5b6b46329f93cda8079c3239771d8cccf2d1d450
kononovich11 <kononovich1703@gmail.com>
2020-03-03 23:42:45 +0300

    work with method  sort

--- app.js
+++ app.js
@@ -320,12 +320,228 @@ console.log(filterUsers(users1, "age", 36));
 [{digit: 1, odd: true}, {digit: 2, odd: false}, {digit: 3, odd: true}...]
 */
 
-const array = [1,2,3,5,8,9,10];
+const array = [
+    1,
+    2,
+    3,
+    5,
+    8,
+    9,
+    10
+];
 const newInfoArr = array.map(element => {
-    return [
-        {
+    return [{
             digit: element,
-            odd: element%2==0
-        }
-    ];
+            odd: element % 2 == 0
+        }];
 });
+
+// Проверить, содержит ли массив [12, 4, 50, 1, 0, 18, 40] элементы, равные нулю. Если да - вернуть true.
+const arrEx = [
+    12,
+    4,
+    50,
+    1,
+    0,
+    18,
+    40
+];
+const isNull = arrEx.some(element => element === 0);
+
+// Проверить, содержит ли массив ['yes', 'hello', 'no', 'easycode', 'what'] хотя бы одно слово длиной больше 3х букв. Если да - вернуть true
+
+const arrayForLen = [
+    'yes',
+    'hello',
+    'no',
+    'easycode',
+    'what'
+];
+const moreThreeLetter = arrayForLen.some(element => element.length > 3);
+
+/*
+Дан массив объектов, где каждый объект содержит информацию о букве и месте её положения в строке {буква: “a”, позиция_в_предложении: 1}:
+
+[{char:"a",index:12}, {char:"w",index:8}, {char:"Y",index:10}, {char:"p",index:3}, {char:"p",index:2},
+{char:"N",index:6}, {char:" ",index:5}, {char:"y",index:4}, {char:"r",index:13}, {char:"H",index:0},
+{char:"e",index:11}, {char:"a",index:1}, {char:" ",index:9}, {char:"!",index:14}, {char:"e",index:7}]
+
+Напишите функцию, которая из элементов массива соберет и вернёт
+строку, основываясь на index каждой буквы. Например:
+[{char:"H",index:0}, {char:"i",index: 1}, {char:"!",index:2}] → “Hi!”
+*/
+
+const arrayInfo = [
+    {
+        char: "a",
+        index: 12
+    },
+    {
+        char: "w",
+        index: 8
+    },
+    {
+        char: "Y",
+        index: 10
+    },
+    {
+        char: "p",
+        index: 3
+    }, {
+        char: "p",
+        index: 2
+    }, {
+        char: "N",
+        index: 6
+    }, {
+        char: " ",
+        index: 5
+    }, {
+        char: "y",
+        index: 4
+    }, {
+        char: "r",
+        index: 13
+    }, {
+        char: "H",
+        index: 0
+    }, {
+        char: "e",
+        index: 11
+    }, {
+        char: "a",
+        index: 1
+    }, {
+        char: " ",
+        index: 9
+    }, {
+        char: "!",
+        index: 14
+    }, {
+        char: "e",
+        index: 7
+    }
+];
+
+function getPhrase(arr) {
+    let str = '';
+    let sortArray = arrayInfo.sort((a, b) => a.index - b.index);
+    sortArray.reduce((prev, item) => str += item['char'], 0);
+    return str;
+}
+console.log(getPhrase(arrayInfo));
+
+/*
+Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы (размер массива определяется его длиной): [  [14, 45],  [1],  ['a', 'c', 'd']  ] → [ [1], [14, 45], ['a', 'c', 'd'] ]
+*/
+let sortArr = [
+    [
+        14, 45
+    ],
+    [1],
+    [
+        'a', 'c', 'd'
+    ]
+];
+sortArr.sort((prev, next) => prev.length - next.length);
+
+/*
+Есть массив объектов:
+[
+    {cpu: 'intel', info: {cores:2, сache: 3}},
+    {cpu: 'intel', info: {cores:4, сache: 4}},
+    {cpu: 'amd', info: {cores:1, сache: 1}},
+    {cpu: 'intel', info: {cores:3, сache: 2}},
+    {cpu: 'amd', info: {cores:4, сache: 2}}
+]
+
+Отсортировать их по возрастающему количеству ядер (cores).
+*/
+
+let coresArr = [
+    {
+        cpu: 'intel',
+        info: {
+            cores: 2,
+            сache: 3
+        }
+    },
+    {
+        cpu: 'intel',
+        info: {
+            cores: 4,
+            сache: 4
+        }
+    },
+    {
+        cpu: 'amd',
+        info: {
+            cores: 1,
+            сache: 1
+        }
+    },
+    {
+        cpu: 'intel',
+        info: {
+            cores: 3,
+            сache: 2
+        }
+    }, {
+        cpu: 'amd',
+        info: {
+            cores: 4,
+            сache: 2
+        }
+    }
+];
+coresArr.sort((prev, next) => prev['info']['cores'] - next['info']['cores']);
+
+/*
+Создать функцию, которая будет принимать массив продуктов и две цены. Функция должна вернуть все продукты, цена которых находится в указанном диапазоне, и сортировать от дешевых к дорогим:
+
+let products = [
+    {title: 'prod1', price: 5.2}, {title: 'prod2', price: 0.18},
+    {title: 'prod3', price: 15}, {title: 'prod4', price: 25},
+    {title: 'prod5', price: 18.9}, {title: 'prod6', price: 8},
+    {title: 'prod7', price: 19}, {title: 'prod8', price: 63}
+];
+
+filterCollection(products, 15, 30) → [{...price: 15}, {...price: 18.9}, {...price: 19}, {...price: 25}]
+*/
+let products = [
+    {
+        title: 'prod1',
+        price: 5.2
+    },
+    {
+        title: 'prod2',
+        price: 0.18
+    },
+    {
+        title: 'prod3',
+        price: 15
+    },
+    {
+        title: 'prod4',
+        price: 25
+    }, {
+        title: 'prod5',
+        price: 18.9
+    }, {
+        title: 'prod6',
+        price: 8
+    }, {
+        title: 'prod7',
+        price: 19
+    }, {
+        title: 'prod8',
+        price: 63
+    }
+];
+
+function filterCollection(arr, price1, price2) {
+    let rangeArr = arr.filter(item => item['price'] >= price1 && item['price'] <= price2).sort((prev, next) => prev['price'] - next['price']);
+    return rangeArr;
+}
+
+const rangePrice = filterCollection(products, 15, 30);
