ec24ebec656f2418db572b8d05534bb384c89896
kononovich11 <kononovich1703@gmail.com>
2020-03-09 00:40:08 +0300

    work with method includes

--- app.js
+++ app.js
@@ -80,3 +80,29 @@ function sum(a) {
 }
 let resSum = sum(5)(-3);
 console.log(resSum);
+
+/*
+У нас есть встроенный метод arr.filter(f) для массивов. Он фильтрует все элементы с помощью функции f. Если она возвращает true, то элемент добавится в возвращаемый массив.
+
+Сделайте набор «готовых к употреблению» фильтров:
+
+inBetween(a, b) – между a и b (включительно).
+inArray([...]) – находится в данном массиве.
+Они должны использоваться таким образом:
+
+arr.filter(inBetween(3,6)) – выбирает только значения между 3 и 6 (включительно).
+arr.filter(inArray([1,2,3])) – выбирает только элементы, совпадающие с одним из элементов массива
+*/
+
+let arrForFilter = [1, 2, 3, 4, 5, 6, 7];
+
+function inBetween(a, b) {
+    return item => item>=a && item<=b;   
+}
+
+function inArray(arr) {
+  return item => arr.includes(item);
+}
+
+let resInBetween = arrForFilter.filter(inBetween(3,6));
+let resInArray = arrForFilter.filter(inArray([1,2,3]));
