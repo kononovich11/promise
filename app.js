cc781fe2988e178ba628d0ed05a1c66868c535a1
kononovich11 <kononovich1703@gmail.com>
2020-03-14 10:54:58 +0300

    filter and sort price

--- app.js
+++ app.js
@@ -276,3 +276,48 @@ const arrOfObjects = [
 ];
 
 arrOfObjects.sort((prev, next) => prev.info.cores - next.info.cores);
+
+/*
+Создать функцию, которая будет принимать массив продуктов и две цены. Функция должна вернуть все продукты, цена которых находится в указанном диапазоне, и сортировать от дешевых к дорогим:
+
+filterCollection(products, 15, 30) → [{...price: 15}, {...price: 18.9}, {...price: 19}, {...price: 25}] */
+
+const products = [
+
+    {
+        title: 'prod1',
+        price: 5.2
+    },
+    {
+        title: 'prod2',
+        price: 0.18
+    },
+
+    {
+        title: 'prod3',
+        price: 15
+    },
+    {
+        title: 'prod4',
+        price: 25
+    }, {
+        title: 'prod5',
+        price: 18.9
+    }, {
+        title: 'prod6',
+        price: 8
+    }, {
+        title: 'prod7',
+        price: 19
+    }, {
+        title: 'prod8',
+        price: 63
+    }
+
+];
+
+function filterCollection(arr, firstPrice, secondPrice) {
+    return arr.filter(item => item.price >= firstPrice && item.price <= secondPrice).sort((prev, next) => prev.price - next.price);
+} 
+
+const res = filterCollection(products, 8, 26);
