5bb9bd237a5a0beec781cefd462b5296d0acc81a
kononovich11 <kononovich1703@gmail.com>
2020-04-11 18:04:54 +0300

    add functions with promise

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const promise = new Promise((resolve, reject) => {
     });
 });
 
-console.log(promise);
+//console.log(promise);
 
 promise.then(x => {
     console.log(x);
@@ -22,4 +22,98 @@ function promiseCreator(time, res) {
 }
 
 const prom = promiseCreator(500, 'ok');
-prom.then(console.log);
+//prom.then(console.log);
+
+//ajax and promise
+function customHttp() {
+    return {
+      get(url, cb) {
+        try {
+          const xhr = new XMLHttpRequest();
+          xhr.open('GET', url);
+          xhr.addEventListener('load', () => {
+            if(Math.floor(xhr.status / 100 ) !==2 ){
+              cb(`Error. Status code: ${xhr.status}`, xhr);
+              return; 
+            }
+            const responseArr = JSON.parse(xhr.responseText);
+            cb(null, responseArr);
+          });
+      
+          xhr.addEventListener('error', () => {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+          });
+      
+          xhr.send();
+        } catch (error) {
+          cb(error);
+        }
+      },
+      posts(url, body, headers, cb) {
+        try {
+          const xhr = new XMLHttpRequest();
+          xhr.open('POST', url);
+          xhr.addEventListener('load', () => {
+            if(Math.floor(xhr.status / 100 ) !==2 ){
+              cb(`Error. Status code: ${xhr.status}`, xhr);
+              return; 
+            }
+            const responseArr = JSON.parse(xhr.responseText);
+            cb(null, responseArr);
+          });
+      
+          xhr.addEventListener('error', () => {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+          });
+  
+          if(headers) {
+            Object.entries(headers).forEach(([key, value]) => {
+              console.log(key, value);
+            });
+          }
+      
+          xhr.send(JSON.stringify(body));
+        } catch (error) {
+          cb(error);
+        }
+      },
+    };
+  } 
+  
+  const http = customHttp();
+  
+
+function getPost(id) {
+    return new Promise((resolve, reject) => {
+        http.get('https://jsonplaceholder.typicode.com/posts/1', (err, res) => {
+            if(err) {
+                reject(err);
+            }
+            resolve(res);
+        });
+    });
+}
+
+function getPostComments() {
+    return new Promise((resolve, reject) => {
+        http.get('https://jsonplaceholder.typicode.com/comments?postId=1', (err, res) => {
+            if(err) {
+                reject(err);
+            }
+            resolve(res);
+        });
+    });
+}
+
+function getUserCreatedPost() {
+    return new Promise((resolve, reject) => {
+        http.get('https://jsonplaceholder.typicode.com/posts?userId=1', (err, res) => {
+            if(err) {
+                reject(err);
+            }
+            resolve(res);
+        });
+    });
+}
+
+getPost().then(posts => console.log(posts));
