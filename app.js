37d1337a764be31389d6a3624454e1ec04b91ea4
kononovich11 <kononovich1703@gmail.com>
2020-04-11 17:34:21 +0300

    add function for promise

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ promise.then(x => {
     console.log(x);
     return x;
 }).then(y => console.log(y)).catch(err => console.log(err));
+
+//Создать функцию, которая возвращает промис.  Функция принимает два аргумента - время, через которое промис должен выполниться, и значение, с которым промис будет выполнен. 
+
+function promiseCreator(time, res) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(res, time);
+        });
+    });
+}
+
+const prom = promiseCreator(500, 'ok');
+prom.then(console.log);
